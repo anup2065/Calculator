6a5d40f588334601dc0d616cd9762bf518372efe
anup2065 <111257141+anup2065@users.noreply.github.com>
2025-09-26 10:21:59 +0545

    initial

---
+++ script.js
@@ -0,0 +1,8 @@
+let num1=$("#num1");
+let num2=$("#num2");
+let result=$("#result ");
+let addbtn=$("#add");
+let subbtn=$("#sub");
+let divbtn=$("#mul");
+let modbtn=$("#div");
+let mulbtn=$("#mod");
