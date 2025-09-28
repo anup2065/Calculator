7232ac4d2edd6c79052841eb3d8afa2d9ab363a4
Anup Ghimire <anup67393@gmail.com>
2025-09-28 13:05:40 +0545

    final done

--- script.js
+++ script.js
@@ -5,4 +5,20 @@ let result=$("#result");
 $("#add").click(function(){
 result.val(parseFloat(a.val())+parseFloat(b.val()));
 });
+
+$("#sub").click(function(){
+    result.val(parseFloat(a.val())-parseFloat(b.val()));
+});
+
+$("#divi").click(function(){
+    result.val(parseFloat(a.val())/parseFloat(b.val()));
+});
+
+$("#mul").click(function(){
+    result.val(parseFloat(a.val())*parseFloat(b.val()));
+});
+
+$("#mod").click(function(){
+    result.val(parseFloat(a.val())%parseFloat(b.val()));
+});
 }); 
