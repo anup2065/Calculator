f9529c3e431ca7b46dbe497d01615de9fa78cadb
Anup Ghimire <anup67393@gmail.com>
2025-09-28 10:32:24 +0545

    addition of css and others

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
+$(document).ready(function(){
 let a=$("#num1");
 let b=$("#num2");
 let result=$("#result");
-let add=$("#add");
-let sub=$("#sub");
-let mul=$("#mul");
-let div=$("#div");
-let mod=$("#mod");
-
-
-$(document).ready(function()
-{
- add.click(function(){
-result.val(parsefloat(a.val())+parsefloat(b.val()));
- }) ;
+$("#add").click(function(){
+result.val(parseFloat(a.val())+parseFloat(b.val()));
 });
+}); 
